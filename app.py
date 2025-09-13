5b5dd767e0f3b244c080e233658801681262e9f5
Renugambal892 <rrenugambal55@gmail.com>
2025-09-13 12:18:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import cv2
 from PIL import Image
 import numpy as np
 from streamlit_drawable_canvas import st_canvas
-import io
-import base64
 
 @st.cache_data
 def load_colors():
@@ -21,12 +19,6 @@ def get_closest_color(R, G, B, df):
             cname = df.loc[i,"color_name"]
     return cname
 
-def get_image_url(image):
-    buffered = io.BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode()
-    return f"data:image/png;base64,{img_str}"
-
 # Load colors
 colors_df = load_colors()
 
@@ -37,16 +29,13 @@ uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
-    # Convert image to base64 URL
-    image_url = get_image_url(image)
-
     st.write("Click on the image to detect a color")
 
     canvas_result = st_canvas(
         fill_color="rgba(0, 0, 0, 0)",
         stroke_width=3,
         stroke_color="#000000",
-        background_image=image_url,
+        background_image=image,
         update_streamlit=True,
         height=image.size[1],
         width=image.size[0],
